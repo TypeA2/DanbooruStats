9ef72684f3bbce0cbb5b8c896e184b36584b02bb
TypeA2 <nuanlim29@gmail.com>
2024-01-04 00:13:48 +0100

    watch for updates using efsw

--- DanbooruStats/web_server.cpp
+++ DanbooruStats/web_server.cpp
@@ -4,10 +4,22 @@
 
 #include <filesystem>
 
-web_server::web_server(const std::string& template_path)
-    : _inja{ template_path } {
+web_server::~web_server() {
+    _watcher.removeWatch(_watch_id);
+}
 
-    spdlog::info("Loading templates from {}", canonical(std::filesystem::path(template_path)).string());
+web_server::web_server(const std::string& template_path)
+    : _template_path{ std::filesystem::canonical(template_path) }
+    , _inja{ template_path /* fs::canonical removes the trailing slash, breaking inja */ } {
+
+    spdlog::info("Loading templates from {}", _template_path.string());
+
+    _watch_id = _watcher.addWatch(_template_path.string(), this, true);
+    if (_watch_id < 0) {
+        throw std::runtime_error{ std::format("Failed to watch {}, error {}", _template_path.string(), _watch_id) };
+    }
+
+    _watcher.watch();
 
     _server.set_logger([](const httplib::Request& req, const httplib::Response& res) {
         spdlog::info("[{}] {} - {}", req.local_addr, req.method, req.path);
@@ -45,7 +57,7 @@ void web_server::listen(const std::string& addr, uint16_t port) {
 }
 
 void web_server::users(int64_t id, const httplib::Request& req, httplib::Response& res) {
-    const auto& tmpl = _ensure_template(cached_template::users, "users.html");
+    const auto& tmpl = _ensure_template(template_id::users);
     inja::json data;
 
     data["user_id"] = id;
@@ -53,12 +65,43 @@ void web_server::users(int64_t id, const httplib::Request& req, httplib::Respons
     res.set_content(_inja.render(tmpl, data), "text/html");
 }
 
-const inja::Template& web_server::_ensure_template(cached_template id, std::string_view file) {
+const inja::Template& web_server::_ensure_template(template_id id) {
     /* Return a template by it's ID, load it from a file if not already loaded */
+    std::string_view file = template_filename(id);
     auto it = _template_cache.find(id);
     if (it == _template_cache.end()) {
+        /* Template does not exist, load and cache it*/
+        
+        _template_paths.emplace(canonical(_template_path / file), id);
         return (_template_cache.emplace(id, _inja.parse_template(std::string{ file })).first)->second;
     }
 
     return it->second;
+}
+
+std::string web_server::_make_template_path(template_id id) const {
+    return canonical(_template_path / template_filename(id)).string();
+}
+
+void web_server::handleFileAction(efsw::WatchID watch_id, const std::string& dir,
+    const std::string& filename, efsw::Action action, std::string old_filename)  {
+    switch (action) {
+        case efsw::Action::Modified: {
+            /* Path relative to template path == template_filename result */
+            auto path = canonical(std::filesystem::path(dir) / filename);
+            if (is_regular_file(path)) {
+                auto it = _template_paths.find(path);
+                
+                if (it != _template_paths.end()) {
+                    spdlog::info("Reloading {}", path.lexically_relative(_template_path).string());
+
+                    /* Reload by just purging the cached template, this avoid reloading unnecessarily */
+                    _template_cache.erase(it->second);
+                }
+            }
+            break;
+        }
+
+        default: break; /* Ignore for now */
+    }
 }
--- DanbooruStats/web_server.h
+++ DanbooruStats/web_server.h
@@ -6,19 +6,27 @@
 
 #include <inja/inja.hpp>
 
+#include <efsw/efsw.hpp>
+
 #include <unordered_map>
 
-class web_server {
+class web_server : private efsw::FileWatchListener {
     httplib::Server _server;
+    std::filesystem::path _template_path;
     inja::Environment _inja;
+    efsw::FileWatcher _watcher;
+    efsw::WatchID _watch_id;
 
-    enum class cached_template {
+    enum class template_id {
         users,
     };
 
-    std::unordered_map<cached_template, inja::Template> _template_cache;
+    std::unordered_map<template_id, inja::Template> _template_cache;
+    std::unordered_map<std::filesystem::path, template_id> _template_paths;
 
     public:
+    virtual ~web_server();
+
     explicit web_server(const std::string& template_path = "./html/");
 
     void listen(const std::string& addr, uint16_t port);
@@ -27,7 +35,18 @@ class web_server {
     virtual void users(int64_t id, const httplib::Request& req, httplib::Response& res);
 
     private:
-    const inja::Template& _ensure_template(cached_template id, std::string_view file);
+    static constexpr std::string_view template_filename(template_id id) {
+        using enum template_id;
+        switch (id) {
+            case users: return "users.html";
+        }
+    }
+
+    const inja::Template& _ensure_template(template_id id);
+    std::string _make_template_path(template_id id) const;
+
+    void handleFileAction(efsw::WatchID watch_id, const std::string& dir,
+        const std::string& filename, efsw::Action action, std::string old_filename) override;
 };
 
 #endif /* WEB_SERVER_H */
